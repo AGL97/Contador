3a8b73fd84e50126ad670ae8fa00f507477d75db
AGL97 <alejandro41.gl@gmail.com>
2024-01-20 22:51:22 -0500

    Initial commit

---
+++ contador.js
@@ -0,0 +1,12 @@
+ 
+ let cont = 0;
+
+ document.querySelector(".increment").onclick = function () {
+    cont+=1;
+    document.querySelector(".num").innerHTML = cont;    
+ }
+
+ document.querySelector(".decrement").onclick = function () {
+    cont-=1;
+    document.querySelector(".num").innerHTML = cont;    
+ }

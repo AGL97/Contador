0fe8b48a8ed2f76072a0ddfa961b5c3488bd6184
AGL97 <alejandro41.gl@gmail.com>
2024-01-20 23:14:38 -0500

    Update contador.js

--- contador.js
+++ contador.js
@@ -4,9 +4,9 @@
  document.querySelector(".increment").onclick = function () {
     cont+=1;
     document.querySelector(".num").innerHTML = cont;    
- }
+ };
 
  document.querySelector(".decrement").onclick = function () {
     cont-=1;
     document.querySelector(".num").innerHTML = cont;    
- }
+ };
